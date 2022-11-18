6b49439685d414f90da212ffcd17fc67506adff2
SampsonM <marksampson70@gmail.com>
2022-11-18 12:11:33 +0000

    added styled components

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  compiler: {
+    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
+    styledComponents: {
+      displayName: true,
+      ssr: true,
+      fileName: true,
+      topLevelImportPaths: [],
+      meaninglessFileNames: ["index"],
+      cssProp: true,
+      namespace: "otb",
+    },
+  }
 }
 
 module.exports = nextConfig
